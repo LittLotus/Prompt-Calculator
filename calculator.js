d3a9f6b1d9dda033cc7a14369e1d96aa197e6c4a
Haleemat <haleematafolayan@gmail.com>
2022-07-16 22:49:23 +0100

    modified

--- calculator.js
+++ calculator.js
@@ -1,10 +1,10 @@
 function myCalculator(){
-    var num1 = Number(prompt("Type of your first number"))
-    
+    var num1 = Number(prompt("Type your first number"))
+
 var operation = prompt("Which operation would you like to perform? Choose one:\n +  -  /  *  %")
 
 
-var num2 = Number(prompt("Type of your second number"))
+var num2 = Number(prompt("Type your second number"))
 
 if (operation == "+"){
     alert("The result is " + (num1+num2))
@@ -16,7 +16,8 @@ else if (operation == "*"){
     alert("The result is " + (num1*num2))
 }
 else if (operation == "/"){
-    alert("The result is " + (num1/num2))
+    let result = num1/num2
+    alert("The result is " + result.toFixed(4) )
 }
 else if (operation == "%"){
     alert("The result is " + (num1 % num2))
